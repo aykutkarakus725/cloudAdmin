e83ff9485269eb79b48708a1cec226da6c8736c4
AYKUT-DT <aykut_pb725q@hotmail.com>
2022-08-03 13:48:37 +0300

    process button

--- src/pages/BookList.js
+++ src/pages/BookList.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import FormDatatable from '../components/FormDatatable'
 import Header from '../components/Header'
+import { Trash, Box } from 'react-feather'
+import { Button } from 'reactstrap'
 
 const BookList = () => {
 
     const columns = [
         {
             name: 'Fullname',
-            maxWidth: "50%",
+            maxWidth: "20%",
             selector: (row) => row.fullname,
             sortable: true,
         },
@@ -16,6 +18,30 @@ const BookList = () => {
             maxWidth: "50%",
             selector: (row) => row.email,
             sortable: true,
+        },
+        {
+            name: "İşlemler",
+            maxWidth: "30%",
+            cell: (row) => (
+                <>
+                    <Button
+                        // onClick={() => deleteApplication(row.id)}
+                        outline
+                        color="danger"
+                        className='mr-2'
+                    >
+                        <Trash size={20} cursor="pointer" />
+                    </Button>
+                    {/* <Button
+                        // onClick={() => updateApplication(row)}
+                        outline
+                        color="info"
+                        className="ml-2"
+                    >
+                        <Box cursor="pointer" size={20} />
+                    </Button> */}
+                </>
+            ),
         },
     ]
 
